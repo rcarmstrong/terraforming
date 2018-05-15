d60453edb64de78202111982936d381f12bf3a45
Ryan Armstrong <rarmstrong@sofi.org>
2018-05-15 15:58:25 -0600

    use s3 remote state.

--- us-west-2/production/vpc/remote_state.tf
+++ us-west-2/production/vpc/remote_state.tf
@@ -1,13 +1,7 @@
-# terraform {
-#   backend "s3" {
-#     bucket = "terraform.rarmstrong-sandbox.com"
-#     key    = "us-west-2/production/vpc/terraform.tfstate"
-#     region = "us-west-2"
-#   }
-# }
-
 terraform {
-  backend "local" {
-    path = "../state/vpc/terraform.tfstate"
+  backend "s3" {
+    bucket = "terraform.rarmstrong-sandbox.com"
+    key    = "us-west-2/production/vpc/terraform.tfstate"
+    region = "us-west-2"
   }
 }
