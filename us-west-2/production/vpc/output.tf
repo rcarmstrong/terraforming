9c1c37d2d32c70d8e1500b3a6d2ce7af6a1e1e56
Ryan Armstrong <rarmstrong@sofi.org>
2018-05-15 16:15:12 -0600

    added more outputs.

--- us-west-2/production/vpc/output.tf
+++ us-west-2/production/vpc/output.tf
@@ -10,6 +10,14 @@ output "public_sg_id" {
   value = "${module.public_sg.id}"
 }
 
+output "private_subnet_ids" {
+  value = "${module.private_subnets.ids}"
+}
+
+output "private_sg_id" {
+  value = "${module.private_sg.id}"
+}
+
 output "nat_gateway_ids" {
   value = "${module.nat_gateways.nat_gateway_ids}"
 }
